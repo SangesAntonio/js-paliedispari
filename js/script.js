033f5ac64b28b69ae1571fafc8ba707cef2e75f1
SangesAntonio <sanges65@gmail.com>
2022-01-14 15:13:21 +0100

    created project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log( 'js ok')
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+const word=prompt('Inserisci una parola', 'Biscotto');
+console.log(word);
+
+function reverseWord(word){
+    let reversedWord
+}
