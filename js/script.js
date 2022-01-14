3a076f3ea93d6bae720b9f787438584c88bbb2a4
SangesAntonio <sanges65@gmail.com>
2022-01-14 15:29:06 +0100

    added reverse function

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ console.log( 'js ok')
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
-const word=prompt('Inserisci una parola', 'Biscotto');
-console.log(word);
+const word1=prompt('Inserisci una parola', 'Biscotto');
+console.log(word1);
 
+//creo funzione per invertire la parola
 function reverseWord(word){
-    let reversedWord
+    let reversedWord;
+    for(let w = word.length - 1; w >= 0; w--){
+        reversedWord += word.charAt(w)
+    }
+    
+    return reversedWord;
 }
+
+const word2=reverseWord(word1);
+console.log(word2)
